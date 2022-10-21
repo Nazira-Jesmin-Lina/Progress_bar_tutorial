a52243633365eb3f621d674f14732a1354c34534
Nazira-Jesmin-Lina <83658996+Nazira-Jesmin-Lina@users.noreply.github.com>
2022-10-21 21:46:38 +0600

    intent add

--- app/src/main/java/com/example/progress_bar_tutorial/MainActivity.java
+++ app/src/main/java/com/example/progress_bar_tutorial/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.progress_bar_tutorial;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnStartProgress;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnStartProgress=findViewById(R.id.button);
+        btnStartProgress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               startActivity(new Intent(MainActivity.this,Next_page.class));
+
+            }
+        });
     }
 }
